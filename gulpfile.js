268388d36d19b5f8c1b9165badf3128238c7655c
Pavel Ševčík <pajaseviwow@gmail.com>
2015-02-15 21:15:50 +0100

    updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,9 @@ var gulp = require('gulp'),
 var paths = {
   jquery: 'bower_components/jquery/dist/jquery.min.js',
   video: 'bower_components/video.js/dist/video-js/video.js',
-  scripts: 'js/main.js',
+  main_scripts: 'js/main.js',
+  index_scripts: 'js/index-scripts.js',
+  garage_scripts: 'js/garage-scripts.js',
   normalize: 'bower_components/normalize-css/normalize.css',
   less: 'less/main.less'
 };
@@ -22,12 +24,26 @@ gulp.task('javascript', function () {
    gulp.src([
      paths.jquery,
      paths.video,
-     paths.scripts
+     paths.main_scripts
      ])
       .pipe(uglify())
       .pipe(concat('main.min.js'))
-      .pipe(gulp.dest('js'))
+      .pipe(gulp.dest('js/build'))
       .pipe(livereload());
+
+    gulp.src([
+        paths.index_scripts
+    ])
+        .pipe(uglify())
+        .pipe(concat('index-scripts.min.js'))
+        .pipe(gulp.dest('js/build'));
+
+    gulp.src([
+        paths.garage_scripts
+    ])
+        .pipe(uglify())
+        .pipe(concat('garage-scripts.min.js'))
+        .pipe(gulp.dest('js/build'));
 });
 
 gulp.task('less', function() {
@@ -45,7 +61,7 @@ gulp.task('less', function() {
 
 gulp.task('watch', ['server'], function() {
   livereload.listen();
-  gulp.watch(paths.scripts, ['javascript']);
+  gulp.watch('js/*', ['javascript']);
   gulp.watch('less/*', ['less']);
 });
 
