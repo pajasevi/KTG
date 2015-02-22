d87c8e9efe991daebdfbcae1fc5fc5f14067baa4
Pavel Ševčík <pajaseviwow@gmail.com>
2015-02-22 16:06:32 +0100

    added skip intro and mute buttons

--- js/index-scripts.js
+++ js/index-scripts.js
@@ -6,52 +6,54 @@ $(function() { // runs after DOM has loaded
   $(window).resize(function () { KTG.resizeToCover(); });
   $(window).trigger('resize');
 
-  var introVideo = videojs('introVideo'),
-      czBlinkVideoOn = videojs('czBlinkVideoOn'),
-      czBlinkVideoOff = videojs('czBlinkVideoOff'),
-      enBlinkVideoOn = videojs('enBlinkVideoOn'),
-      enBlinkVideoOff = videojs('enBlinkVideoOff'),
-      enParkVideo = videojs('enParkVideo');
+  videos = {
+      introVideo : videojs('introVideo'),
+      czBlinkVideoOn : videojs('czBlinkVideoOn'),
+      czBlinkVideoOff : videojs('czBlinkVideoOff'),
+      enBlinkVideoOn : videojs('enBlinkVideoOn'),
+      enBlinkVideoOff : videojs('enBlinkVideoOff'),
+      enParkVideo : videojs('enParkVideo')
+  }
 
   // Intro video
 
-  introVideo.play();
+  videos.introVideo.play();
 
-  introVideo.on('ended', function() {
+  videos.introVideo.on('ended', function() {
     KTG.appendFirst();
   });
 
   // CZ blink videos
 
-  czBlinkVideoOn.on('ended', function() {
+  videos.czBlinkVideoOn.on('ended', function() {
     $('#czBlinkVideoOn').fadeOut( 100 );
     $('#czBlinkVideoOff').fadeIn( 100 );
   });
 
-  czBlinkVideoOff.on('ended', function() {
+  videos.czBlinkVideoOff.on('ended', function() {
     $('#czBlinkVideoOff').fadeOut( 100, function() {
-        czBlinkVideoOn.currentTime( 0 );
-        czBlinkVideoOff.currentTime( 0 );
+        videos.czBlinkVideoOn.currentTime( 0 );
+        videos.czBlinkVideoOff.currentTime( 0 );
     });
   });
 
   // EN blink videos
 
-  enBlinkVideoOn.on('ended', function() {
+  videos.enBlinkVideoOn.on('ended', function() {
     $('#enBlinkVideoOn').fadeOut( 100 );
     $('#enBlinkVideoOff').fadeIn( 100 );
   });
 
-  enBlinkVideoOff.on('ended', function() {
+  videos.enBlinkVideoOff.on('ended', function() {
     $('#enBlinkVideoOff').fadeOut( 100, function() {
-        enBlinkVideoOn.currentTime( 0 );
-        enBlinkVideoOff.currentTime( 0 );
+        videos.enBlinkVideoOn.currentTime( 0 );
+        videos.enBlinkVideoOff.currentTime( 0 );
     });
   });
 
   // Parking videos
 
-  enParkVideo.on('ended', function() {
+  videos.enParkVideo.on('ended', function() {
     window.location.href += "cz-garage.html";
   });
 
@@ -59,33 +61,60 @@ $(function() { // runs after DOM has loaded
 
   // User Events
 
+  // Skip Intro
+  $('.skip-intro').on('click', function( event ) {
+      event.preventDefault();
+
+      videos.introVideo.pause();
+      videos.introVideo.currentTime(45);
+      videos.introVideo.play();
+  });
+
+  // Mute all sounds
+  $('.mute-button').on('click', function( event ) {
+      event.preventDefault();
+
+      if( $(this).hasClass('mute-on') ) {
+          $.each(videos, function( key, value ) {
+              value.muted(true);
+          });
+          $(this).removeClass('mute-on').addClass('mute-off');
+      }
+      else {
+          $.each(videos, function( key, value ) {
+              value.muted(false);
+          });
+          $(this).removeClass('mute-off').addClass('mute-on');
+      }
+  });
+
   $('.lang-link.cz').on('mouseover', function() {
     $('#czBlinkVideoOn').fadeIn( 100 , function() {
-      czBlinkVideoOn.play();
+      videos.czBlinkVideoOn.play();
     });
   });
 
   $('.lang-link.cz').on('mouseout', function() {
-    czBlinkVideoOff.play();
+      videos.czBlinkVideoOff.play();
   });
 
   $('.lang-link.en').on('mouseover', function() {
     $('#enBlinkVideoOn').fadeIn( 100 , function() {
-      enBlinkVideoOn.play();
+        videos.enBlinkVideoOn.play();
     });
   });
 
   $('.lang-link.en').on('mouseout', function() {
     $('#enBlinkVideoOff').fadeIn( 100 , function() {
-      enBlinkVideoOff.play();
+        videos.enBlinkVideoOff.play();
     });
   });
 
   $('.lang-link').on('click', function( event ) {
     event.preventDefault();
     $('#enParkVideo').fadeIn( 100 , function() {
-      enParkVideo.play();
-      KTG.hideFirst();
+        videos.enParkVideo.play();
+        KTG.hideFirst();
     });
   });
 
